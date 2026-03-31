a597c9b63b93890eb76a4ba869b2f43b9f337448
garrettkolson <garrett@smallwerks.com>
2026-03-31 10:51:43 -0600

    added missing db migrations

---
+++ src/ClaudeManager.Hub/Persistence/Migrations/20260331155135_AddLlmDeploymentHealthColumns.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClaudeManager.Hub.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLlmDeploymentHealthColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
